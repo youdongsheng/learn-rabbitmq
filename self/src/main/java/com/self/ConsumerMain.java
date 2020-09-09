f5be5f44e5cd9da0281cf944353c79dc966e97f7
you <Abcd1234>
2020-09-09 23:25:46 +0800

    update

--- self/src/main/java/com/self/ConsumerMain.java
+++ self/src/main/java/com/self/ConsumerMain.java
@@ -16,13 +16,22 @@ public class ConsumerMain {
     public static final String self_direct_queue = "self_direct_queue";
     public static final String self_direct_routingKey = "self_direct_routingKey";
     public static void main(String[] args) throws Exception {
+        consume();
+    }
+
+    public static void consume() throws Exception{
+        // 1、通过address方式 创建连接
         Address[] addresses = new Address[]{new Address("127.0.0.1",5672)};
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setUsername("guest");
         connectionFactory.setPassword("guest");
         Connection connection = connectionFactory.newConnection(addresses);
+
+        // 2、创建channel
         final Channel channel = connection.createChannel();
+        // 3、设置消息获取的数量
         channel.basicQos(64);
+        // 4、创建消费者
         Consumer consumer = new DefaultConsumer(channel){
             @Override
             public void handleDelivery(String consumerTag, Envelope envelope, AMQP.BasicProperties properties, byte[] body) throws IOException {
@@ -37,8 +46,11 @@ public class ConsumerMain {
 
             }
         };
+
+        // 5、消费者 订阅 queue
         channel.basicConsume(self_direct_queue, consumer);
-        TimeUnit.SECONDS.sleep(50000);
+        TimeUnit.SECONDS.sleep(50000); //休眠保证消费者能够完全消费消息
+        // 6、关闭channel和connection
         channel.close();
         connection.close();
     }
--- self/src/main/java/com/self/ProducerMain.java
+++ self/src/main/java/com/self/ProducerMain.java
@@ -1,14 +1,13 @@
 package com.self;
 
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.MessageProperties;
+import com.rabbitmq.client.*;
 
 import java.io.IOException;
+import java.rmi.server.ExportException;
 import java.util.concurrent.TimeoutException;
 
 /**
+ *
  * @author: yds
  * @date: 2020/09/08
  */
@@ -16,7 +15,35 @@ public class ProducerMain {
     public static final String self_direct_exchange = "self_direct_exchange";
     public static final String self_direct_queue = "self_direct_queue";
     public static final String self_direct_routingKey = "self_direct_routingKey";
-    public static void main(String[] args) throws IOException, TimeoutException {
+    public static void main(String[] args) throws Exception {
+        deleteQueue();
+    }
+
+    public static void sendMsg() throws Exception {
+        // 1、创建connection
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        Connection connection = connectionFactory.newConnection();
+        // 2、创建channel
+        Channel channel = connection.createChannel();
+        // 3、创建 exchange
+        channel.exchangeDeclare(self_direct_exchange,"direct", true,true,null);
+        // 4、创建queue
+        channel.queueDeclare(self_direct_queue,false,false,false,null);
+        // 5、将 exchange 和 queue 进行绑定
+        channel.queueBind(self_direct_queue,self_direct_exchange,self_direct_routingKey);
+        // 6、发送message，携带routingKey 到指定的exchange
+        String message = "hello world";
+        channel.basicPublish(self_direct_exchange,self_direct_routingKey, MessageProperties.TEXT_PLAIN,message.getBytes("UTF-8"));
+        // 7、关闭channel和connection
+        channel.close();
+        connection.close();
+    }
+
+    public static void deleteExchange() throws Exception {
         ConnectionFactory connectionFactory = new ConnectionFactory();
         connectionFactory.setHost("127.0.0.1");
         connectionFactory.setPort(5672);
@@ -24,11 +51,21 @@ public class ProducerMain {
         connectionFactory.setPassword("guest");
         Connection connection = connectionFactory.newConnection();
         Channel channel = connection.createChannel();
-        channel.exchangeDeclare(self_direct_exchange,"direct");
-        channel.queueDeclare(self_direct_queue,false,false,false,null);
-        channel.queueBind(self_direct_queue,self_direct_exchange,self_direct_routingKey);
-        String message = "hello world";
-        channel.basicPublish(self_direct_exchange,self_direct_routingKey, MessageProperties.TEXT_PLAIN,message.getBytes("UTF-8"));
+        AMQP.Exchange.DeleteOk result = channel.exchangeDelete(self_direct_exchange);
+        System.out.println(result);
+        channel.close();
+        connection.close();
+    }
+
+    public static void deleteQueue() throws Exception {
+        ConnectionFactory connectionFactory = new ConnectionFactory();
+        connectionFactory.setHost("127.0.0.1");
+        connectionFactory.setPort(5672);
+        connectionFactory.setUsername("guest");
+        connectionFactory.setPassword("guest");
+        Connection connection = connectionFactory.newConnection();
+        Channel channel = connection.createChannel();
+        channel.queueDelete(self_direct_queue);
         channel.close();
         connection.close();
     }
